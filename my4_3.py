922047e67f872d85860a38a3612fef6c308d9420
seoeugenee <88884056+seoeugenee@users.noreply.github.com>
2021-09-01 01:29:21 +0900

    ch 4 구현 완료
    ch 4 구현 완료

--- my4_3.py
+++ my4_3.py
@@ -1,2 +1,17 @@
 string = input()
-row = 
+row = int(string[1])
+column = int(ord(string[0])) - int(ord('a')) + 1
+result = 0
+list = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
+
+for i in range(8):
+    r = row + list[i][0]  #이동한 위치는 새 변수에서 확인 row, column은 고정
+    c = column + list[i][1]
+    #if r < 1 or r > 8 or c < 1 or c > 8:  #가장 처음의 위치는 고정인데 잘못 생각
+        #r -= list[i][0]
+        #c -= list[i][1]
+    #elif 1<= r <= 8 and 1<= c <= 8:
+    if 1<= r <= 8 and 1<= c <= 8:
+        result += 1
+    
+print(result)
---
+++ my4_4.py
@@ -0,0 +1,3 @@
+n, m = map(int, input().split())
+a, b, d = map(int, input().split())
+#이 이후로는 코드를 어떻게 짜야 할지 감이 안와서 답안 보고 이해함
